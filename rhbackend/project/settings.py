e7d514eb26e652ac123fcbb7c438cc11653aa7e5
Marlene Nunes <isabeldias817@gmail.com>
2025-01-25 18:34:20 +0000

    Add `graphene_django` lib to `INSTALLED_APPS` in settings file

--- rhbackend/project/settings.py
+++ rhbackend/project/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # third-party libs
+    "graphene_django",
 ]
 
 MIDDLEWARE = [
