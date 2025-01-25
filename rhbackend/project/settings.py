4c7396230c681bb866b5fa640bc434f9f84dfd01
Marlene Nunes <isabeldias817@gmail.com>
2025-01-25 20:53:51 +0000

    Add job app

--- rhbackend/project/settings.py
+++ rhbackend/project/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     # local apps
     'candidate.apps.CandidateConfig',
+    'job.apps.JobConfig',
     # third-party libs
     "graphene_django",
 ]
